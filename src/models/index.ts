6d4deefe595734f5896ca7e9d621ead4e90a3023
BuckleMashu <cuongnguyenlvl30@gmail.com>
2024-07-28 21:41:02 +0800

    added based for Share/Request pages

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,13 @@
 export type ToDoItem = {
     id: number;
     value: string;
-  };
+};
+
+export type share_page = {
+    id: number;
+    type: number;
+    title: string;
+    picture: string;
+    tags: string;
+    address: string;
+}
